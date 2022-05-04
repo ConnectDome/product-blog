0b1a6ac15603018353f3beb70af2ee034fe0658e
Arth Tyagi <arthtyagi7@gmail.com>
2022-05-04 13:37:55 +0530

    v1 for connectdome product blog

---
+++ components/JoinButton.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import styles from './styles.module.css';
+
+export const JoinButton: React.FC = () => {
+// add a join button or idk, we could just use notion to have that join button
+}
